aa4917339c2dac383644598d438eae0fece5178b
Alessio Scicolone <scicolone.alessio@gmail.com>
2013-12-02 17:47:34 +0100

    Init()

--- tga-tsp/src/org/tgatsp/Cliente.java
+++ tga-tsp/src/org/tgatsp/Cliente.java
@@ -1,5 +1,11 @@
 package org.tgatsp;
 
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 //import java.util.*;
 //import java.math.*;
 
@@ -45,5 +51,65 @@ public class Cliente
 	public String toString()
 	{
 		return ""+id;
+	}
+	
+	public static void init(String file)
+	{
+		
+		//String type;
+		int dimension;//Size di Tour
+		//String edge_weight_type;
+		try
+		{	
+	
+				FileReader f;
+				f=new FileReader(file);
+    
+				BufferedReader b;
+				b=new BufferedReader(f);	
+				
+				String str;
+				
+				str=b.readLine();
+				String[] str_arr= str.split(": ");
+				TGA.nome=str_arr[1];
+				str=b.readLine();
+				str_arr= str.split(": ");
+				TGA.comment=str_arr[1];
+				b.readLine();
+				//str_arr= str.split(": ");
+				//type=str_arr[1];
+				str=b.readLine();
+				str_arr= str.split(": ");
+				dimension=Integer.parseInt(str_arr[1]);
+				b.readLine();
+				//str_arr= str.split(": ");
+				//edge_weight_type=str_arr[1];
+				//Lettura a vuoto
+				b.readLine();	
+				Cliente c;
+				Coordinates cr;
+				Cliente.listaClienti=new Cliente[dimension];
+				
+				for (int i = 0; i < dimension; i++) 
+				{					
+					str=b.readLine();
+					str_arr= str.split(" ");
+					cr=new Coordinates(Float.parseFloat(str_arr[2]),Float.parseFloat(str_arr[3]));
+					
+						int k= Integer.parseInt(str_arr[1]);
+						c=new Cliente(k,cr);
+						Cliente.listaClienti[i]=c;
+						//System.out.println(Cliente.listaClienti[i]);		
+					
+				}
+		}catch(FileNotFoundException e)
+			{
+				e.printStackTrace();
+			}
+		catch(IOException io)
+		{
+			io.printStackTrace();
+		}
 	}
 }
